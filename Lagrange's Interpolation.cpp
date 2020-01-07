96fe823b6c45b919ff4d45f77d5cd8f8cee2c42a
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2020-01-07 22:11:45 +0600

    Create Lagrange's Interpolation.cpp

---
+++ Lagrange's Interpolation.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define dd double
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define mx 200005
+#define mod 1000000007
+#define fr first
+#define cti(a) (a-48)
+#define itc(a) char(a+48)
+#define se second
+#define End cout<<"\n"
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define memo(Array,val) memset(Array, val, sizeof Array)
+#define check(n, pos) (n & (1<<pos))
+#define Set(n, pos) (n | (1<<pos))
+ld X[mx],Y[mx],sum=0;
+ll n,x;
+
+void solve()
+{
+    for(ll i=0; i<n; i++){
+        ld lob=1,hor=1;
+        for(ll j=0; j<n; j++){
+            if(i == j) continue;
+            lob*=(x-X[j]);
+            hor*=(X[i]-X[j]);
+        }
+        sum+=((lob/hor)*Y[i]);
+    }
+    cout << "Value of f(" << x << ") is: " << sum << "\n";
+}
+
+void Input()
+{
+   cout << "Enter Number of Elements(Xn, Yn): ";
+   cin >> n;
+   End;
+   for(ll i=0; i<n; i++){
+       cout << "Enter Value of X[" << i << "] = ";
+       cin >> X[i];
+   }
+   End;
+   for(ll i=0; i<n; i++){
+       cout << "Enter Value of Y[" << i << "] = ";
+       cin >> Y[i];
+   }
+   cout << "\nFor f(x) give the Value of x: ";
+   cin >> x;
+
+   solve();
+
+}
+
+int main()
+{
+   //freopen("text.txt","r",stdin);
+   Input();
+   return 0;
+}
+
